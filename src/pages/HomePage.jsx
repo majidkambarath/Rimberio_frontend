fd268cbaeb52536d74cbe60e5b71a149dc41d8c3
ABDUL MAJID K <majidmaji3410@gmail.com>
2024-01-20 11:39:21 +0530

    bug fixed in landingPage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import LandingPage from '../components/LandingPage'
+import LandingPage from '../components/landingPage/Home'
 export default function Home() {
   return (
     <div><LandingPage/></div>
--- src/router/UserRouter.jsx
+++ src/router/UserRouter.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
-import Home from "../pages/Home";
+import HomePage from "../pages/HomePage";
 import Product from "../pages/Product";
 import CartPage from "../pages/CartPage";
 export default function UserRouter() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path="/" element={<HomePage />} />
       <Route path="/product" element={<Product />} />
       <Route path="/cart" element={<CartPage />} />
     </Routes>
